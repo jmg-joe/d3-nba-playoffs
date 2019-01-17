c235e6cb864d3c2f8740eed073cbf35ce94c348b
jmg-joe <gagliano.joe1@gmail.com>
2019-01-16 20:13:57 -0600

    clean up js file

--- scripts/shots.js
+++ scripts/shots.js
@@ -3,40 +3,26 @@ d3.csv("../data/playoff_shots.csv", function(data){
 
     var shots = d3.select("svg")
         .selectAll("g")
-            .data(data)
-            .enter()
-            .append("g")
-                .attr("class", "shot")
-                    .attr("transform", function(d){
-                        return "translate(" + ((1.5 * d.LOC_Y) + 50) + "," + ((1.5 * d.LOC_X) + 370) + ")";
-                    })
-            .on("mouseenter", function(d){
-                d3.select(this).raise()
-                    .append("text")
-                    .attr("class", "playername")
-                    .text(d.PLAYER_NAME)
-            })
-            .on("mouseleave", function(d){
-                d3.selectAll("text.playername").remove();
-            })
-            .on("click", function(d){
-                $('#myModal').modal();
-                d3.select(".modal-header")
-                    .text(d.PLAYER_NAME)
-                d3.select(".modal-body").append("p").text()
-            })
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "shot")
+        .attr("transform",  function(d){
+            return translateCoordsOnSvg(d)
+        })
+
+        .on("mouseenter", function (d) {
+            addPlayerDescription(this, d)
+        })
+        .on("mouseleave", function (d) {
+           removePlayerDescription(d)
+        })
+        .on("click", function (d) {
+            triggerPlayerModal(d)
+        })
             
-    shots.append("circle")
-        .attr("r", 4)
-           .attr("fill", function(d){
-               if (d.EVENT_TYPE == "Made Shot" ) {
-                    return "green"
-                
-               }else{
-                   return "red"
-               }
-               
-           })
+    appendShotsToSVG(shots, 4);
+
     var players = d3.nest()
         .key(function(d){ return d.PLAYER_NAME; })
         .rollup(function(a){ return a.length; })
@@ -46,10 +32,8 @@ d3.csv("../data/playoff_shots.csv", function(data){
     var playoffGames = d3.nest()
            .key(function(d){ return d.GAME_ID })
            .entries(data)
-    console.log(playoffGames);
 
     players.unshift({"key": "ALL", "value": d3.sum(players, function(d) {return d.value;})});
-    // playoffGames.unshift({"key":"All Games", "value": d3.sum(playoffGames, function(d){ return d.value;})});
     var selector = d3.select("#selector");
     var gameSelector = d3.select('#gameSelector');
     selector
@@ -61,11 +45,10 @@ d3.csv("../data/playoff_shots.csv", function(data){
            .property('value', function(d){ return d.key })
 
     selector   
-        .on("change", function(){
+        .on("change", function(d){
             d3.selectAll(".shot")
                 .attr("opacity", "1.0");
             var value = selector.property("value");
-            console.log(value);
             if (value != "ALL"){
                 d3.selectAll(".shot")
                     .filter(function(d){ return d.PLAYER_NAME != value; })
@@ -94,5 +77,57 @@ d3.csv("../data/playoff_shots.csv", function(data){
                     .attr("opacity", "0.1")
             }
         })
-    console.log(players);
 })
+
+// put shots data onto svg canvas
+function appendShotsToSVG(obj, radius){
+    obj.append("circle")
+    .attr("r", radius)
+    .attr("fill", function(d){
+        if (d.EVENT_TYPE == "Made Shot") {
+            return "green"
+        } else {
+            return "red"
+        }
+    });
+}
+
+// transform data based on svg on DOM
+function translateCoordsOnSvg(d) {
+    return "translate(" + ((1.5 * d.LOC_Y) + 50) + "," + ((1.5 * d.LOC_X) + 370) + ")";
+}
+
+
+//event listeners start
+function addPlayerDescription(obj,d){
+    d3.select(obj).raise()
+            .append("text")
+            .attr("class", "playername")
+            .text(d.PLAYER_NAME)
+}
+
+function removePlayerDescription(){
+    d3.selectAll("text.playername").remove()
+}
+
+
+function triggerPlayerModal(d){
+    $('#myModal').modal();
+    d3.select(".modal-header")
+        .text(d.PLAYER_NAME)
+    d3.select(".modal-body").append("p").text("test")
+}
+//event listeners end
+
+
+
+function work(d){
+    d3.selectAll(".shot")
+                .attr("opacity", "1.0");
+            var value = selector.property("value");
+            if (value != "ALL"){
+                d3.selectAll(".shot")
+                    .filter(function(d){ return d.PLAYER_NAME != value; })
+                    .attr("opacity", "0.1");
+            }
+}
